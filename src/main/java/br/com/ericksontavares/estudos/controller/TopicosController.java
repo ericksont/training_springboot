e23e92da6861dbbf5a237be096119f3afb77cd90
Erickson Tavares <etavares@unifor.br>
2022-02-08 13:31:07 -0300

    finalizando api para implementacao dos topico

--- src/main/java/br/com/ericksontavares/estudos/controller/TopicosController.java
+++ src/main/java/br/com/ericksontavares/estudos/controller/TopicosController.java
@@ -2,14 +2,18 @@ package br.com.ericksontavares.estudos.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.ericksontavares.estudos.controller.dto.DetalhesDoTopicoDTO;
 import br.com.ericksontavares.estudos.controller.dto.TopicoDTO;
+import br.com.ericksontavares.estudos.controller.form.AtualizacaoTopicoForm;
 import br.com.ericksontavares.estudos.controller.form.TopicoForm;
 import br.com.ericksontavares.estudos.modelo.Topico;
 import br.com.ericksontavares.estudos.repository.CursoRepository;
@@ -44,18 +49,49 @@ public class TopicosController {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm topicoForm, UriComponentsBuilder uriBuilder){
         Topico topico = topicoForm.converter(cursoRepository);
         repository.save(topico);
-
         URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new TopicoDTO(topico));
     }
 
     @GetMapping("/{id}")
-    public DetalhesDoTopicoDTO detalhar(@PathVariable Long id){
-        Topico topico = repository.getById(id);
-        return new DetalhesDoTopicoDTO(topico);
+    public ResponseEntity<DetalhesDoTopicoDTO> detalhar(@PathVariable Long id){
+        Optional<Topico> topico = repository.findById(id);
+        if(topico.isPresent()) {
+            return ResponseEntity.ok(new DetalhesDoTopicoDTO(topico.get()));
+        } 
+        
+        return ResponseEntity.notFound().build();
+        
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm topicoForm){
+        Optional<Topico> optional = repository.findById(id);
+        if(optional.isPresent()) {
+            Topico topico = topicoForm.atualizar(id, repository);
+            return ResponseEntity.ok(new TopicoDTO(topico));
+        } 
+        
+        return ResponseEntity.notFound().build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id){
+        Optional<Topico> optional = repository.findById(id);
+        if(optional.isPresent()) {
+            repository.deleteById(id);
+            return ResponseEntity.ok().build();
+        } 
+        
+        return ResponseEntity.notFound().build();
+        
     }
     
 }
